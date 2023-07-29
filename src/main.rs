4c1e2bfd7c8eb274e29e226c0150742239a55419
Jessie Chatham Spencer <jessie@teainspace.com>
2023-07-29 18:39:39 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -43,161 +43,160 @@ pub enum TSType {
     String,
     Number,
     Boolean,
+    Function,
 }
 
 #[derive(Debug)]
 pub struct TSIdentifier(String);
 
 #[derive(Debug)]
-pub enum TSAST {
-    Value(TSValue),
-    Expression(TSExpression),
-    Assignment(TSIdentifier, TSExpression),
-}
-
-#[derive(Debug)]
 pub enum TypedAst {
     Value(TSValue, TSType),
     Expression(TSExpression),
     Assignment(TSIdentifier, TSExpression),
+    Function(TSIdentifier, Vec<TypedAst>),
 }
 
 fn main() {
-    // let parsed_res = TSParser::parse(Rule::program, "let myvar = \"test\"; myvar").unwrap();
+    let parsed_res =
+        TSParser::parse(Rule::program, "fn test () {  let myvar = \"test\"; myvar }").unwrap();
 
-    // let mut ast: Vec<TypedAst> = vec![];
-
-    // for rule in parsed_res {
-    //     let node = match rule.as_rule() {
-    //         Rule::program => todo!(),
-    //         Rule::expression => TypedAst::Expression(parse_expression(rule).unwrap()),
-    //         Rule::assignment => parse_assignment(rule).unwrap(),
-    //         _ => continue,
-    //     };
-
-    //     ast.push(node);
-    // }
-
-    // let registry = DialectRegistry::new();
-    // register_all_dialects(&registry);
-
-    // let context = Context::new();
-    // context.append_dialect_registry(&registry);
-    // context.load_all_available_dialects();
-    // register_all_llvm_translations(&context);
-
-    // let location = Location::unknown(&context);
-    // let mut module = Module::new(location);
-
-    // let index_type = Type::index(&context);
-    // register_all_passes();
-
-    // let pass_manager = PassManager::new(&context);
-    // pass_manager.enable_verifier(true);
-    // pass_manager.add_pass(pass::conversion::create_arith_to_llvm());
-    // pass_manager.add_pass(pass::conversion::create_control_flow_to_llvm());
-    // pass_manager.add_pass(pass::conversion::create_func_to_llvm());
-    // pass_manager.add_pass(pass::conversion::create_index_to_llvm_pass());
-    // pass_manager.add_pass(pass::conversion::create_mem_ref_to_llvm());
-    // pass_manager.add_pass(pass::conversion::create_reconcile_unrealized_casts());
-
-    // pass_manager
-    //     .nested_under("func.func")
-    //     .add_pass(pass::conversion::create_arith_to_llvm());
-
-    let context = create_test_context();
-
-    let mut module = Module::parse(
-        &context,
-        r#"
-            module {
-                func.func @add(%arg0 : i32) -> i32 attributes { llvm.emit_c_interface } {
-                    %res = arith.addi %arg0, %arg0 : i32
-                    return %res : i32
-                }
-            }
-            "#,
-    )
-    .unwrap();
-
-    println!("atts: {:?}", &module.body().first_operation().unwrap());
+    let registry = DialectRegistry::new();
+    register_all_dialects(&registry);
+    let context = Context::new();
+    context.append_dialect_registry(&registry);
+    context.get_or_load_dialect("func");
+    context.get_or_load_dialect("arith");
+    register_all_llvm_translations(&context);
 
     let location = Location::unknown(&context);
     let mut module = Module::new(location);
 
-    let index_type = Type::index(&context);
-    let mut module = Module::new(location);
-    module.body().append_operation(func::func(
-        &context,
-        StringAttribute::new(&context, "add"),
-        TypeAttribute::new(
-            FunctionType::new(&context, &[index_type, index_type], &[index_type]).into(),
-        ),
-        {
-            let block = Block::new(&[(index_type, location), (index_type, location)]);
+    let mut ast: Vec<TypedAst> = vec![];
 
-            let sum = block.append_operation(arith::addi(
-                block.argument(0).unwrap().into(),
-                block.argument(1).unwrap().into(),
-                location,
-            ));
+    for rule in parsed_res {
+        let node = match rule.as_rule() {
+            Rule::program => todo!(),
+            Rule::expression => TypedAst::Expression(parse_expression(rule).unwrap()),
+            Rule::assignment => parse_assignment(rule).unwrap(),
+            Rule::function => {
+                println!("function: {}", rule);
 
-            block.append_operation(func::r#return(&[sum.result(0).unwrap().into()], location));
+                let mut inner = rule.into_inner();
 
-            let region = Region::new();
-            region.append_block(block);
-            region
-        },
-        &[],
-        location,
-    ));
+                let identifer = TSIdentifier(inner.next().unwrap().as_str().to_string());
+
+                println!("id: {:?}", identifer);
+
+                let mut fn_body = vec![];
+
+                while let Some(body_exp) = inner.next() {
+                    let node = match body_exp.as_rule() {
+                        Rule::expression => {
+                            TypedAst::Expression(parse_expression(body_exp).unwrap())
+                        }
+                        Rule::assignment => parse_assignment(body_exp).unwrap(),
+                        _ => todo!(),
+                    };
+
+                    fn_body.push(node);
+                }
+
+                TypedAst::Function(identifer, fn_body)
+            }
+            _ => continue,
+        };
+
+        ast.push(node);
+    }
+
+    println!("ast: {:?}", ast);
+
+    for node in ast {
+        match node {
+            TypedAst::Function(id, body) => {
+               for node in ast {
+                    
+                } 
+            }
+            _ =>
+        }
+    }
+
+    let integer_type = IntegerType::new(&context, 64);
+
+    let function = {
+        let region = Region::new();
+        let block = Block::new(&[
+            (integer_type.into(), location),
+            (integer_type.into(), location),
+        ]);
+        let arg1 = block.argument(0).unwrap().into();
+        let arg2 = block.argument(1).unwrap().into();
+
+        let sum = block.append_operation(arith::addi(arg1, arg2, location));
+
+        block.append_operation(func::r#return(&[sum.result(0).unwrap().into()], location));
+
+        region.append_block(block);
+        func::func(
+            &context,
+            StringAttribute::new(&context, "add"),
+            TypeAttribute::new(
+                FunctionType::new(
+                    &context,
+                    &[integer_type.into(), integer_type.into()],
+                    &[integer_type.into()],
+                )
+                .into(),
+            ),
+            region,
+            &[(
+                Identifier::new(&context, "llvm.emit_c_interface"),
+                Attribute::unit(&context),
+            )],
+            location,
+        )
+    };
+
+    module.body().append_operation(function);
+
+    assert!(module.as_operation().verify());
 
     let pass_manager = pass::PassManager::new(&context);
+    register_all_passes();
+    pass_manager.enable_verifier(true);
+    pass_manager.add_pass(pass::conversion::create_control_flow_to_llvm());
     pass_manager.add_pass(pass::conversion::create_func_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_index_to_llvm_pass());
+    pass_manager.add_pass(pass::conversion::create_mem_ref_to_llvm());
+    pass_manager.add_pass(pass::conversion::create_reconcile_unrealized_casts());
 
     pass_manager
         .nested_under("func.func")
         .add_pass(pass::conversion::create_arith_to_llvm());
-
-    assert_eq!(pass_manager.run(&mut module), Ok(()));
+    pass_manager.run(&mut module).unwrap();
 
     let engine = ExecutionEngine::new(&module, 2, &[], false);
 
-    let mut argument = 42;
-    let mut result = -1;
+    let mut argument1: i64 = 2;
+    let mut argument2: i64 = 4;
+    let mut result: i64 = -1;
 
     unsafe {
-        println!(
-            "res: {:?}",
-            engine
-                .invoke_packed(
-                    "add",
-                    &mut [
-                        &mut argument as *mut i32 as *mut (),
-                        &mut result as *mut i32 as *mut (),
-                    ],
-                )
-                .unwrap()
-        )
+        engine
+            .invoke_packed(
+                "add",
+                &mut [
+                    &mut argument1 as *mut i64 as *mut (),
+                    &mut argument2 as *mut i64 as *mut (),
+                    &mut result as *mut i64 as *mut (),
+                ],
+            )
+            .unwrap();
     };
 
-    println!("res: {}", result);
-
-    // pass_manager.run(&mut module).unwrap();
-
-    // assert!(module.as_operation().verify());
-
-    // let engine = ExecutionEngine::new(&module, 3, &[], true);
-
-    // engine.dump_to_object_file("test.mlir");
-    // unsafe {
-    //     println!(
-    //         "{:?}",
-    //         engine.invoke_packed("add", &mut [1 as *mut (), 2 as *mut ()])
-    //     )
-    // };
-
-    //println!("ast {:?}", module.to_raw());
+    assert_eq!(result, 6);
 }
 
 fn parse_assignment(assignment: Pair<Rule>) -> Result<TypedAst, Box<dyn std::error::Error>> {
