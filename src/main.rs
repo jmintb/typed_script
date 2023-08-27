41b30f1e61586e06ab281b2ab955621818251c34
Jessie Chatham Spencer <jessie@teainspace.com>
2023-08-27 20:27:07 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::iter::FlatMap;
+use std::{iter::FlatMap, sync::mpsc::TrySendError};
 
+use mlir_sys::mlirLLVMArrayTypeGet;
 use pest::{iterators::Pair, Parser};
 use pest_derive::{self, Parser};
 
@@ -10,7 +11,7 @@ use melior::{
         DialectRegistry,
     },
     ir::{
-        attribute::{FlatSymbolRefAttribute, StringAttribute, TypeAttribute},
+        attribute::{ArrayAttribute, FlatSymbolRefAttribute, StringAttribute, TypeAttribute},
         operation::OperationBuilder,
         r#type::{FunctionType, IntegerType, MemRefType},
         Location, *,
@@ -31,6 +32,7 @@ struct TSParser;
 #[derive(Debug)]
 pub enum TSExpression {
     Value(TSValue),
+    Call(TSIdentifier, Vec<TSExpression>),
 }
 
 #[derive(Debug)]
@@ -62,8 +64,8 @@ pub enum TypedAst {
 
 fn main() {
     let parsed_res = TSParser::parse(
-        Rule::function,
-        "fn test () { 
+        Rule::program,
+        "fn main () { 
             printf(\"hello\");
          }",
     )
@@ -83,7 +85,10 @@ fn main() {
 
     let mut ast: Vec<TypedAst> = vec![];
 
-    for rule in parsed_res {
+    let statements = parsed_res.into_iter();
+
+    for rule in statements {
+        println!("statement: {:?}", rule.as_rule());
         let node = match rule.as_rule() {
             Rule::program => todo!(),
             Rule::expression => TypedAst::Expression(parse_expression(rule).unwrap()),
@@ -93,23 +98,14 @@ fn main() {
 
                 let identifer = TSIdentifier(inner.next().unwrap().as_str().to_string());
 
-                let mut fn_body = vec![];
+                let body = inner
+                    .map(|exp| TypedAst::Expression(parse_expression(exp).unwrap()))
+                    .collect();
 
-                while let Some(body_exp) = inner.next() {
-                    let node = match body_exp.as_rule() {
-                        Rule::expression => {
-                            TypedAst::Expression(parse_expression(body_exp).unwrap())
-                        }
-                        Rule::assignment => parse_assignment(body_exp).unwrap(),
-                        _ => todo!(),
-                    };
-
-                    fn_body.push(node);
-                }
-
-                TypedAst::Function(identifer, fn_body)
+                TypedAst::Function(identifer, body)
             }
-            _ => continue,
+            Rule::EOI => break,
+            _ => panic!("unexpected rule {:?}", rule.as_rule()),
         };
 
         ast.push(node);
@@ -126,39 +122,93 @@ fn main() {
 
     let integer_type = IntegerType::new(&context, 64);
 
-    let function = {
-        let region = Region::new();
-        let block = Block::new(&[
-            (integer_type.into(), location),
-            (integer_type.into(), location),
-        ]);
-        let arg1 = block.argument(0).unwrap().into();
-        let arg2 = block.argument(1).unwrap().into();
+    for node in ast {
+        if let TypedAst::Function(id, body) = node {
+            let region = Region::new();
+            let block = Block::new(&[]);
 
-        let sum = block.append_operation(arith::addi(arg1, arg2, location));
+            for exp in body {
+                match exp {
+                    TypedAst::Expression(TSExpression::Call(id, args)) => {
+                        let TSExpression::Value(TSValue::String(ref val)) =
+                            args[0] else {todo!()};
 
-        block.append_operation(func::r#return(&[sum.result(0).unwrap().into()], location));
+                        let arr_type =
+                            llvm::r#type::array(IntegerType::new(&context, 32).into(), 2 as u32);
+                        let op = OperationBuilder::new("somestir", location)
+                            .add_attributes(&[(
+                                Identifier::new(&context, "value"),
+                                // ArrayAttribute::new(&context, &[]).into(),
+                                StringAttribute::new(&context, &"dl").into(),
+                            )])
+                            .enable_result_type_inference()
+                            .build();
 
-        region.append_block(block);
-        func::func(
-            &context,
-            StringAttribute::new(&context, "add"),
-            TypeAttribute::new(
-                FunctionType::new(
-                    &context,
-                    &[integer_type.into(), integer_type.into()],
-                    &[integer_type.into()],
-                )
-                .into(),
-            ),
-            region,
-            &[(
-                Identifier::new(&context, "llvm.emit_c_interface"),
-                Attribute::unit(&context),
-            )],
-            location,
-        )
-    };
+                        func::call(
+                            &context,
+                            FlatSymbolRefAttribute::new(&context, "printf"),
+                            &[op.result(0).unwrap().into()],
+                            &[Type::none(&context)],
+                            location,
+                        );
+
+                        block.append_operation(op);
+                    }
+                    _ => todo!(),
+                }
+            }
+
+            region.append_block(block);
+
+            let function = func::func(
+                &context,
+                StringAttribute::new(&context, &id.0),
+                TypeAttribute::new(FunctionType::new(&context, &[], &[]).into()),
+                region,
+                &[(
+                    Identifier::new(&context, "llvm.emit_c_interface"),
+                    Attribute::unit(&context),
+                )],
+                location,
+            );
+
+            module.body().append_operation(function);
+        }
+    }
+
+    // let function = {
+    //     let region = Region::new();
+    //     let block = Block::new(&[
+    //         (integer_type.into(), location),
+    //         (integer_type.into(), location),
+    //     ]);
+    //     let arg1 = block.argument(0).unwrap().into();
+    //     let arg2 = block.argument(1).unwrap().into();
+
+    //     let sum = block.append_operation(arith::addi(arg1, arg2, location));
+
+    //     block.append_operation(func::r#return(&[sum.result(0).unwrap().into()], location));
+
+    //     region.append_block(block);
+    //     func::func(
+    //         &context,
+    //         StringAttribute::new(&context, "add"),
+    //         TypeAttribute::new(
+    //             FunctionType::new(
+    //                 &context,
+    //                 &[integer_type.into(), integer_type.into()],
+    //                 &[integer_type.into()],
+    //             )
+    //             .into(),
+    //         ),
+    //         region,
+    //         &[(
+    //             Identifier::new(&context, "llvm.emit_c_interface"),
+    //             Attribute::unit(&context),
+    //         )],
+    //         location,
+    //     )
+    // };
 
     let printf_decl = llvm::func(
         &context,
@@ -189,54 +239,54 @@ fn main() {
         location,
     );
 
-    let region = Region::new();
-    let block = Block::new(&[(
-        llvm::r#type::pointer(IntegerType::new(&context, 8).into(), 0).into(),
-        location,
-    )]);
+    // let region = Region::new();
+    // let block = Block::new(&[(
+    //     llvm::r#type::pointer(IntegerType::new(&context, 8).into(), 0).into(),
+    //     location,
+    // )]);
 
-    let arg1 = block.argument(0).unwrap().into();
-    unsafe {
-        let res = block.append_operation(
-            OperationBuilder::new("llvm.call", location)
-                .add_operands(&[arg1])
-                .add_attributes(&[(
-                    Identifier::new(&context, "callee"),
-                    FlatSymbolRefAttribute::new(&context, "printf").into(),
-                )])
-                .add_results(&[IntegerType::new(&context, 32).into()])
-                .build(),
-        );
-        block.append_operation(llvm::r#return(
-            Some(res.result(0).unwrap().into()),
-            location,
-        ));
-    }
+    // let arg1 = block.argument(0).unwrap().into();
+    // unsafe {
+    //     let res = block.append_operation(
+    //         OperationBuilder::new("llvm.call", location)
+    //             .add_operands(&[arg1])
+    //             .add_attributes(&[(
+    //                 Identifier::new(&context, "callee"),
+    //                 FlatSymbolRefAttribute::new(&context, "printf").into(),
+    //             )])
+    //             .add_results(&[IntegerType::new(&context, 32).into()])
+    //             .build(),
+    //     );
+    //     block.append_operation(llvm::r#return(
+    //         Some(res.result(0).unwrap().into()),
+    //         location,
+    //     ));
+    // }
 
-    region.append_block(block);
-    let print_decl = llvm::func(
-        &context,
-        StringAttribute::new(&context, "print"),
-        TypeAttribute::new(
-            llvm::r#type::function(
-                IntegerType::new(&context, 32).into(),
-                &[llvm::r#type::pointer(IntegerType::new(&context, 8).into(), 0).into()],
-                false,
-            )
-            .into(),
-            // FunctionType::new(&context, &[r#type::Type::none(&context)], &[]).into(),
-        ),
-        region,
-        &[(
-            Identifier::new(&context, "llvm.emit_c_interface"),
-            Attribute::unit(&context),
-        )],
-        location,
-    );
+    // region.append_block(block);
+    // let print_decl = llvm::func(
+    //     &context,
+    //     StringAttribute::new(&context, "print"),
+    //     TypeAttribute::new(
+    //         llvm::r#type::function(
+    //             IntegerType::new(&context, 32).into(),
+    //             &[llvm::r#type::pointer(IntegerType::new(&context, 8).into(), 0).into()],
+    //             false,
+    //         )
+    //         .into(),
+    //         // FunctionType::new(&context, &[r#type::Type::none(&context)], &[]).into(),
+    //     ),
+    //     region,
+    //     &[(
+    //         Identifier::new(&context, "llvm.emit_c_interface"),
+    //         Attribute::unit(&context),
+    //     )],
+    //     location,
+    // );
 
     module.body().append_operation(printf_decl);
-    module.body().append_operation(function);
-    module.body().append_operation(print_decl);
+    // module.body().append_operation(function);
+    // module.body().append_operation(print_decl);
 
     assert!(module.as_operation().verify());
 
@@ -275,7 +325,7 @@ fn main() {
     unsafe {
         engine
             .invoke_packed(
-                "print",
+                "main",
                 &mut [ptr as *mut (), &mut result as *mut i32 as *mut ()],
             )
             .unwrap();
@@ -300,15 +350,29 @@ fn parse_assignment(assignment: Pair<Rule>) -> Result<TypedAst, Box<dyn std::err
 }
 
 fn parse_expression(expression: Pair<Rule>) -> Result<TSExpression, Box<dyn std::error::Error>> {
-    let mut inner_rules = expression.into_inner().next().unwrap();
+    let inner_rules = expression.clone().into_inner().next().unwrap();
 
-    let typed_exp = match inner_rules.as_rule() {
+    let typed_exp = match expression.as_rule() {
         Rule::string => TSExpression::Value(TSValue::String(inner_rules.as_str().into())),
         Rule::identifier => TSExpression::Value(TSValue::Variable(inner_rules.as_str().into())),
-        _ => todo!(),
+        Rule::call => parse_fn_call(expression)?,
+        _ => panic!("Got unexpected expression: {:?}", expression.as_rule()),
     };
 
     Ok(typed_exp)
+}
+
+fn parse_fn_call(call_expression: Pair<Rule>) -> Result<TSExpression, Box<dyn std::error::Error>> {
+    let mut inner = call_expression.clone().into_inner().into_iter();
+
+    println!("inner {call_expression:?}");
+    let id = inner.next().unwrap();
+    let args = inner.map(|arg| parse_expression(arg).unwrap()).collect();
+
+    Ok(TSExpression::Call(
+        TSIdentifier(id.as_str().to_string()),
+        args,
+    ))
 }
 
 pub fn load_all_dialects(context: &Context) {
