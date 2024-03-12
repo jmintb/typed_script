38e2d9b96b5a28efd2845646bcfd2559ee2fb4b9
Jessie Chatham Spencer <jessie@teainspace.com>
2024-03-12 15:20:22 +0100

    Add test

---
+++ ir_test_programs/test_loop_borrow_and_move.ts
@@ -0,0 +1,15 @@
+fn main() {
+  let val = "test"
+  let i = 0
+
+  while (i) < (10) {
+    let val = "test2";
+    i = (i) + (1) 
+    borrow_and_move(val, i)
+
+  }
+}
+
+fn borrow_and_move(owned input: str, j: integer) {
+    
+}
