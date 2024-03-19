51d2a24a104f4b6e668009ffafad49e2dcad1ac0
Jessie Chatham Spencer <jessie@teainspace.com>
2024-03-19 16:03:24 +0100

    Update test snapshots

--- ir_test_programs/test_borrow_existing_variable_after_loop.ts
+++ ir_test_programs/test_borrow_existing_variable_after_loop.ts
@@ -1,10 +1,17 @@
 fn main() {
   let test = "test";
   while (1) > (0) {
-    borrow_val(test)
+
+    if (1) > (0) {
+      print(test)
+    } else {
+      borrow_val(test)
+    }
+    
+    borrow_vall(test)
   }
 
-  borrow_val(test)
+  borrow_valll(test)
 }
 
 fn move_val(owned test: str) {
@@ -12,5 +19,13 @@ fn move_val(owned test: str) {
 }
 
 fn borrow_val(test: str) {
+  
+}
+
+fn borrow_vall(test: str) {
+  
+}
+
+fn borrow_valll(test: str) {
   
 }
