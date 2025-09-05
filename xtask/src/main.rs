01f0c984322cc4d37bbf6eb04be2ea277c1a86bf
Jessie Chatham Spencer <jessie@jessiecspencer.com>
2025-09-05 18:18:48 +0000

    Introduce cargo xtask for project scripts

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,40 +1,91 @@
-use std::env::{args, current_dir, set_current_dir};
-use std::path::PathBuf;
+//! This crate hosts adhoc scripts using the [xtask](https://github.com/matklad/cargo-xtask)
+//! pattern.
+//! Think of it as an alternative to makefile or just, using cargo and rust.
+//!
+//! This allows us to write custom tooling for working with the Fusion project without relying on
+//! external tools. We also get to write the tools in rust instead of bash or similar. Making
+//! this crossplatform and easier to maintain.
+//!
+//! The strategy is to keep this is simple as possible with next to no external dependencies.
+//! As the requirements grow we can grow the crate a long with it.
+//!
+//! Current it only houses commands for performing code quality checks.
+
+use std::env::args;
 use std::process;
 
 type Error = Box<dyn std::error::Error>;
-
 type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
     let args: Vec<String> = args().collect();
 
     if args.len() < 2 {
-        println!("please one of the following commands: [lint]");
+        println!("please provide one of the following commands: [check, fix]");
         return Ok(());
     }
 
-    let mut parent_dir: PathBuf = current_dir()?;
-
-    set_current_dir(parent_dir)?;
-
     let command = &args[1];
 
-    if command == "lint" {
-        println!("linting project");
-        process::Command::new("cargo")
-            .args(["fmt", "--all"])
-            .spawn()?
-            .wait();
-
-        process::Command::new("cargo")
-            .args(["clippy", "--fix"])
-            .spawn()?
-            .wait();
+    if command == "check" {
+        run_code_checks()?;
+    } else if command == "fix" {
+        run_code_checks_and_apply_fixes()?;
     }
+
     Ok(())
 }
 
-fn print_help_message() {
-    println!();
+fn run_code_checks() -> Result<()> {
+    println!("Running code quality checks!");
+
+    println!("Running rustfmt");
+    process::Command::new("cargo")
+        .args(["fmt", "--all", "--check"])
+        .spawn()?
+        .wait()?;
+
+    println!("Running clippy");
+    process::Command::new("cargo")
+        .args(["clippy"])
+        .spawn()?
+        .wait()?;
+
+    println!("Running cargo check");
+    process::Command::new("cargo")
+        .args(["check", "--workspace", "--all-targets", "--all-features"])
+        .spawn()?
+        .wait()?;
+
+    Ok(())
+}
+
+fn run_code_checks_and_apply_fixes() -> Result<()> {
+    println!("Applying code quality fixes!");
+
+    println!("Running rustfmt");
+    process::Command::new("cargo")
+        .args(["fmt", "--all"])
+        .spawn()?
+        .wait()?;
+
+    println!("Running clippy");
+    process::Command::new("cargo")
+        .args(["clippy", "--fix", "--allow-dirty"])
+        .spawn()?
+        .wait()?;
+
+    println!("Running cargo fix");
+    process::Command::new("cargo")
+        .args([
+            "fix",
+            "--workspace",
+            "--all-targets",
+            "--all-features",
+            "--allow-dirty",
+        ])
+        .spawn()?
+        .wait()?;
+
+    Ok(())
 }
